eb3a90ddac0ef6076d171e2428ece5b51654c37d
stevenvuong00 <steven.vuong00@gmail.com>
2022-09-21 23:06:34 -0400

    tried to import lol

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("Hello")
+import numpy as np
+import json
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+file_path = "goemotions.json.gz"
+with open(file_path, "r") as data:
+    data_set = json.load(data)
