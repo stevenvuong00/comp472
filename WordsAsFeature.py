c18b6743045a2b1df2583370b711d9fa806f7006
Be An <amqkvo@gmail.com>
2022-09-23 16:43:08 -0400

    Multinomial Naive Bayes Classifier

--- WordsAsFeature.py
+++ WordsAsFeature.py
@@ -1,13 +1,40 @@
 from DatasetPreparation import json_load, emotionsList, sentimentsList
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 
-wordsList =  [list[0] for list in json_load]
 
+commentsList =  [list[0] for list in json_load]
 vec = CountVectorizer()
-vec_fit = vec.fit_transform(wordsList)
+commentsVectorized = vec.fit_transform(commentsList)
 
+# 2.1
 vocabulary =  vec.get_feature_names_out()
-vocabCount = np.asarray(vec_fit.sum(axis=0))[0]
-
+vocabCount = np.asarray(commentsVectorized.sum(axis=0))[0]
 print(dict(zip(vocabulary, vocabCount)))
+
+# 2.2
+training_set, test_set = train_test_split(commentsList, test_size=0.20, random_state=77)
+
+emotionsVectorized = vec.fit_transform(emotionsList)
+emotions = vec.get_feature_names_out()
+
+emotionsDict = {}
+for i, emotion in enumerate(emotions):
+    emotionsDict[emotion] = i
+
+print(emotionsDict)
+emotionsY = []
+
+for i, data in enumerate(json_load):
+    e = data[1]
+    emotionsY.append(emotionsDict[e])
+
+classifier = MultinomialNB()
+model = classifier.fit(vec.fit_transform(commentsList), emotionsY)
+
+test_comment = vec.transform(np.array(['i love you']))
+
+predict = model.predict(test_comment)
+print(predict)
