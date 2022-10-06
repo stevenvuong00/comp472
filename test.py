ead6a3ae0700b871deb4bef59108aa89807a1de2
stevenvuong00 <steven.vuong00@gmail.com>
2022-10-06 18:47:31 -0400

    preprocessing for decision trees

--- test.py
+++ test.py
@@ -19,14 +19,14 @@ maxSublist = max(arr1, key=len) # returns the longest sub list
 print(sizeArr1)
 print(maxSublist)
 
-# padding the 
+# padding the arrays
 a = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
 b = np.zeros([len(a),len(max(a, key = lambda x: len(x)))])
 for i, j in enumerate(a):
     b[i][0:len(j)] = j
 
-print(a)
-print(b)
+# print(a)
+# print(b)
 
 # print ("1st array : ", arr1)  
 # print ("2nd array : ", arr2)  
@@ -38,3 +38,50 @@ print(b)
 # pad with "" until the end
 # then readd the emotion and sentiment
 # now we have ndarray of m x m 
+
+# https://stackoverflow.com/questions/47666539/numpy-zeros-and-string-data-type blank but in ''
+
+# simulate json_load
+arr3 = [['Taking a blood thinner such as aspirin would keep it from clotting', 'approval', 'positive'], ['I am glad that the message touched you, I really am.', 'gratitude', 'positive']]
+# np_arr3 = np.array(arr3)
+
+# get all the last 2 elements of the data set
+targets_emotion = [element[1] for element in arr3]
+print(targets_emotion)
+
+targets_sentiment = [element[2] for element in arr3]
+print(targets_sentiment)
+
+# list of only the words without label
+only_words = [element[0] for element in arr3]
+print(only_words)
+
+# transform each sublist into a list of list
+words_list = [element.split() for element in only_words]
+print(words_list)
+
+# get max length of a sublist, that's the length number of features (words)
+maxLenSublist = max(words_list, key=len)
+print("max sublength: ")
+print(len(maxLenSublist))
+
+# pad the lists
+# dtype='<U25' accept all strings length 25 --> creating fresh 2d np array with the needed length
+# could change U25
+padded_words = np.zeros((len(arr3), len(maxLenSublist)), dtype='<U25')
+
+# adding all the words to the new array
+for i in range(len(arr3)):
+    for j in range(len(words_list[i])):
+        # need to prevent index uot of bound, depends on the length of the orinigal word list
+        padded_words[i][j] = words_list[i][j]
+print(padded_words)
+
+# re-adding the emotion and sentiments
+training_set = padded_words.copy().tolist()
+for i in range(len(training_set)):
+    training_set[i].append(targets_emotion[i])
+    training_set[i].append(targets_sentiment[i])
+
+# print(targets_emotion[0])
+print(training_set)
